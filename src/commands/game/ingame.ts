a3dea3af31c8cf152248e002c491f4a88084db05
zaid450 <me@zaid-arshad.tech>
2020-09-18 21:37:26 -0400

    fix(ingame): yeahh

--- src/commands/game/ingame.ts
+++ src/commands/game/ingame.ts
@@ -6,11 +6,6 @@ class InGameCommand extends Command {
     super('game-ingame', {
       channel: 'guild',
       args: [
-        {
-          id: 'unmuteAll',
-        type: 'flag',
-          flag: "--unmute"
-          },
           {
               id: "over",
               type: "flag",
@@ -19,28 +14,24 @@ class InGameCommand extends Command {
       ],
     });
   }
-    async exec( message: Message, { unmuteAll, over }: { unmuteAll: boolean; over: boolean; } ) {
-        if (!message.member?.voice.channel)
-            return message.channel.send(
-            'Bruh, you need to be in a voice channel for this to work'
-            );
-        const channel = message.member.voice.channel;
-        if ( over ) {
-            this.client.config.mutedPeople = [];
-            for (const member of channel.members.values()) {
-                await member.voice.setMute( false );
-            }
-            return message.channel.send( "Game has ended, onto the next hopefully!" );
-        } else {
-            for ( const member of channel.members.values() ) {
-                unmuteAll
-                  ? !this.client.config.mutedPeople.includes(member.id)
-                    ? await member.voice.setMute(false)
-                    : null
-                  : await member.voice.setMute(true);
-            }
-            return message.channel.send( `All members have been ${unmuteAll ? "unmuted" : "muted"}` );
+    async exec( message: Message, { over }: { over: boolean; } ) {
+      if (!message.member?.voice.channel)
+          return message.channel.send(
+          'Bruh, you need to be in a voice channel for this to work'
+      );
+      const channel = message.member.voice.channel;
+      if ( over ) {
+        for (const member of channel.members.values()) {
+          await member.voice.setMute(false);
         }
+      } else {
+        for ( const member of channel.members.values() ) {
+          await member.voice.setMute( true );
+        }
+      }
+      return message.channel.send(
+        `All members have been ${over ? 'unmuted' : 'muted'}`
+      );
     }
 }
 
